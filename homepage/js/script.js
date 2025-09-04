f021c598bfb734eebeb3b2bcecaa0a73abdef84f
Hui-Hua, Hsiao <yaya650042@gmail.com>
2025-09-04 20:58:27 +0800

    done ver.1

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -4,4 +4,11 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
-  
+
+// Toggle nav-links visibility
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
